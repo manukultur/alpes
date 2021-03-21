0854db4101c518c99304354aa399c044b371bc5e
Christopher O'Connor <coconnor@manukultur.com>
2021-03-21 12:35:05 +0100

    fix spacing on mobile

--- slices/ImageMarquee.js
+++ slices/ImageMarquee.js
@@ -5,7 +5,7 @@ import Marquee from "react-fast-marquee";
 export default function ImageTicker({ slice }) {
   return (
     <SliceFrame width="full" vertical="none">
-      <Marquee speed={160} className="" pauseOnHover>
+      <Marquee speed={160} className="" pauseOnHover gradientWidth={20}>
         {slice.items.map((item, index) => (
           <div className="flex flex-col justify-end h-full w-96">
             <Image
